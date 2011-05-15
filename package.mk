dd2d5db3fe6ad5d079fb3adb6276d7467addd61f
Alvaro Videla <videlalvaro@gmail.com>
2011-05-15 13:14:33 +0200

    fixes package.mk to include the priv directory

--- package.mk
+++ package.mk
@@ -1,1 +1,6 @@
 DEPS:=rabbitmq-server misultin_wrapper rabbitmq-erlang-client
+
+CONSTRUCT_APP_PREREQS:=$(shell find $(PACKAGE_DIR)/priv -type f)
+define construct_app_commands
+	cp -r $(PACKAGE_DIR)/priv $(APP_DIR)
+endef
